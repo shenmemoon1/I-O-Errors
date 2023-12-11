ce35f26669c54f58f4de7d8fd47e5f2b29ce9bd5
billlims <davidlinxf@gmail.com>
2023-12-11 18:30:21 +1100

    navbar and active future

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ function App() {
     <main>
       <Router>
         <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/frontend" element={<FrontEnd />} />
-          <Route path="/flutter" element={<Flutter />} />
-          <Route path="/dotnet" element={<DotNet />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
+        <div className="mt-16">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/frontend" element={<FrontEnd />} />
+            <Route path="/flutter" element={<Flutter />} />
+            <Route path="/dotnet" element={<DotNet />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
       </Router>
     </main>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,17 +3,65 @@ import { farmer } from "../assets";
 
 const Nav = () => {
   return (
-    <div className=" w-full h-10">
-      <NavLink to="/">
-        <img src={farmer} />
-      </NavLink>
-      <NavLink to="/frontend">FrontEnd</NavLink>
-      <NavLink to="/flutter">Flutter</NavLink>
-      <NavLink to="/dotnet">DotNet</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/signup">SignUp</NavLink>
-    </div>
+    <header className="header">
+      <div className="flex justify-center align-middle">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `px-6 py-3 ${isActive ? "text-blue-500" : "text-black"}`
+          }
+        >
+          <img src={farmer} className="w-6 h-6" />
+        </NavLink>
+        <NavLink
+          to="/frontend"
+          className={({ isActive }) =>
+            `px-6 py-3 ${isActive ? "text-blue-500" : "text-black"}`
+          }
+        >
+          FrontEnd
+        </NavLink>
+        <NavLink
+          to="/flutter"
+          className={({ isActive }) =>
+            `px-6 py-3 ${isActive ? "text-blue-500" : "text-black"}`
+          }
+        >
+          Flutter
+        </NavLink>
+        <NavLink
+          to="/dotnet"
+          className={({ isActive }) =>
+            `px-6 py-3 ${isActive ? "text-blue-500" : "text-black"}`
+          }
+        >
+          DotNet
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            `px-6 py-3 ${isActive ? "text-blue-500" : "text-black"}`
+          }
+        >
+          About
+        </NavLink>
+      </div>
+      <div className="flex justify-center align-middle gap-3">
+        <NavLink
+          to="/login"
+          className="relative flex items-center justify-center px-6 py-2 rounded-full shadow-md text-white bg-gradient-to-r from-purple-500 to-indigo-500"
+        >
+          <span className="absolute w-full h-full top-0 left-0 border-2 border-gradient rounded-full"></span>
+          Login
+        </NavLink>
+        <NavLink
+          to="/signup"
+          className="relative flex items-center justify-center px-6 py-2 rounded-full shadow-md border-solid border-2 border-indigo-500 text-indigo-500"
+        >
+          SignUp
+        </NavLink>
+      </div>
+    </header>
   );
 };
 
