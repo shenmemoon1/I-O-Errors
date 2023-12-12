40c08ee3446dc25de2b5dacc02a0977fb33e7023
billlims <davidlinxf@gmail.com>
2023-12-12 20:09:50 +1100

    signup setup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,12 @@
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <section>
+      <div className="font-mono text-gray-500 text-lg box mx-auto mt-5">
+        Welcome to I/O Erros! <br />
+        Lets begin the adventure
+      </div>
+    </section>
+  );
 };
 
 export default SignUp;
